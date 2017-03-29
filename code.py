c4ea037f7439653925c444ed9f1a9b02d68dd702
Harsh Gupta <mail@hargup.in>
2017-03-29 18:09:55 +0530

    Add credit and fix plot
    Signed-off-by: Harsh Gupta <mail@hargup.in>

--- code.py
+++ code.py
@@ -177,95 +177,46 @@ qs = [
         ]
 
 
-code_license_questions = [
+code_license_questions = [qs[49], qs[30], qs[31]]
 
- "To your understanding, through which of the following ways has your enterprise protected its own mobile app products",
- "How often do you license your mobile app code for your enterprise upon sharing it with others or in the app store",
- "How do you license your code upon sharing it"
-        ]
+design_license_questions = [qs[40], qs[41], qs[42], qs[43]]
 
-
-design_license_questions = [
-    "How often do you license your designs upon sharing them with others or in the app store",
-    "How do you license your designs",
-    "What would your enterprise like others to be able to do with your designs",
-    "What would you ideally require from others in order to be able to do such things with your designs"
-        ]
-
-
-reverse_engineering_practicies = [
-    "For what reason(s) have you\xa0reverse engineered / decompiled apps in the past",
-    "How often have you used parts of code accessed by reverse engineering / decompilation in your work"
-        ]
-
-
-influence = [
-    "Who or what influences your enterprise's decisions related to ownership, contracts, licensing, and/or protection of your works",
-    "Through which of the following ways do you think your enterprise would like to protect their mobile app products ideally in the future",
-    "If your enterprise is interested in protecting its products, what are the main reasons for doing so"
- ]
-
+reverse_engineering_practicies = [qs[37], qs[38]]
 
 df = pd.read_csv("survey_results.csv")
-# I should probably drop all nan columns
 df = df.dropna(axis=1, how='all')
 
-
-verticles = {
-    "app developer": (df['Which area of expertise best describes your personal work currently? [Mobile app development]'] == "Yes"),
-    "app designer": (df['Which area of expertise best describes your personal work currently? [Mobile app design]'] == "Yes"),
-    "product/team manager": df['Which area of expertise best describes your personal work currently? [Product / Team management]'] == "Yes"
-        }
+# verticles = get_conds(df, qs[2])
 
 
 def num_yes(bools):
     return Counter(bools)[True]
 
-# for name, cond in verticles.items():
-#     print("{}: {}".format(name, num_yes(cond)))
-#
-# for n1, n2 in combinations(verticles.keys(), 2):
-#     print("{} and {}: {}".format(n1, n2, num_yes(verticles[n1] & verticles[n2])))
-
-# for ques in code_license_questions:
-#     print_answers_verticles(df, ques, verticles)
-
-
-# for ques in design_license_questions:
-#     print_answers_verticles(df, ques, verticles)
-
-
-# for ques in reverse_engineering_practicies:
-#     print_answers_verticles(df, ques, verticles)
-
-q49 = "To your understanding, through which of the following ways has your enterprise protected its own mobile app products"
-q50 = "Through which of the following ways do you think your enterprise would like to protect their mobile app products ideally in the future"
-
 
 def plot_bar():
-    # TODO: The lables needs be bigger and they need to be cleaned
-    ans_q49 = get_answers(df, q49)
-    ans_q50 = get_answers(df, q50)
+    # Credit: http://stackoverflow.com/a/10369955/1780891
+    ans_q49 = get_answers(df, qs[49])
+    ans_q50 = get_answers(df, qs[50])
 
     N = len(ans_q49)
 
-    q49vals = [x["Yes"] for x in ans_q49.values()]
-    q50vals = [x["Yes"] for x in ans_q50.values()]
+    q49vals = [x.setdefault("Yes", 0) for x in ans_q49.values()]
+    q50vals = [x.setdefault("Yes", 0) for x in ans_q50.values()]
 
     ind = np.arange(N)*3  # the x locations for the groups
     width = np.min(np.diff(ind))/3       # the width of the bars
 
-    fig = plt.figure(figsize=(30,10))
+    fig = plt.figure(figsize=(30, 10))
     ax = fig.add_subplot(111)
     b1 = ax.bar(np.arange(N)*3 + 1, q49vals, color='b', alpha=0.5)
     b2 = ax.bar(np.arange(N)*3, q50vals, color='r', alpha=0.5)
 
-    # add some
     ax.set_ylabel('Scores')
-    ax.set_title('Scores by group and gender')
+    ax.set_title('\"Ways used to protect the mobile app\" vs \"Ideal way to protect mobile app\"')
     ax.set_xticks(ind+width)
     ax.set_xticklabels(map(clean_ans, ans_q49.keys()))
 
     ax.legend((b1[0], b2[0]), ('Q49', 'Q50'))
+    fig.autofmt_xdate()
 
     plt.show()
